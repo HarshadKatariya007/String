1d984d3acecda94b16d7655bd76ea10e4ec27221
Harshad Katariya <159020392+HarshadKatariya007@users.noreply.github.com>
2024-02-08 17:16:59 +0530

    Add files via upload

---
+++ String/String.js
@@ -0,0 +1,14 @@
+
+let str="Harshad";
+
+function print(n)
+{
+    console.log(str.length);
+    console.log(str.split("a"));
+    console.log(str.replace("H",("A")));
+    console.log(str.includes("Ha"));
+    console.log(str.indexOf("a"));
+    console.log(str.toLowerCase());
+    console.log(str.toUpperCase());
+}
+print();
